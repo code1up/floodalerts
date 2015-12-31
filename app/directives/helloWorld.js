ccba694e8e69e0d779cee7e4c082b4e95567f054
code1up <dev.code1up@googlemail.com>
2015-12-31 15:14:25 +0000

    remove dead TODO

--- app/directives/helloWorld.js
+++ app/directives/helloWorld.js
@@ -11,7 +11,6 @@ module.exports = function (app) {
         return {
             restrict: "E",
             replace: true,
-            // TODO: https://egghead.io/lessons/angularjs-angular-with-webpack-requiring-templates
             template: require("./helloWorld.html"),
             scope: {
                 name: "@"
